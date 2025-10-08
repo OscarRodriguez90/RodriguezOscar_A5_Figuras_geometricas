43113d21a7926648f4a68d96ea1ed717f66f87bd
OscarR <461853.joan23@fje.edu>
2025-10-08 20:26:34 +0200

    Clase Cuadrado

---
+++ clases/Cuadrado.php
@@ -0,0 +1,29 @@
+<?php
+<?php
+
+require_once 'FiguraGeometrica.php';
+
+class Cuadrado extends FiguraGeometrica {
+
+    public function __construct($lado1) {
+        parent::__construct('Cuadrado', $lado1);
+    }
+
+    public function __destruct() {
+        // Destructor vacío
+    }
+
+    public function calcularArea() {
+        return $this->getLado1() * $this->getLado1();
+    }
+
+    public function calcularPerimetro() {
+        return 4 * $this->getLado1();
+    }
+
+    public function __toString() {
+        return "Cuadrado de lado " . $this->getLado1() .
+               ". Área: " . $this->calcularArea() .
+               ". Perímetro: " . $this->calcularPerimetro();
+    }
+}
